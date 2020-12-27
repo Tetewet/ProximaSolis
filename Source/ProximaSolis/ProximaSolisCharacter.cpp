9613748fc990fc6c8a139e4cc06d9129289cba45
Théophile Gorisse <theogorisse@live.com>
2020-12-27 02:16:55 -0500

    Add Character Model

--- Source/ProximaSolis/ProximaSolisCharacter.cpp
+++ Source/ProximaSolis/ProximaSolisCharacter.cpp
@@ -28,7 +28,7 @@ AProximaSolisCharacter::AProximaSolisCharacter()
 	// Configure character movement
 	GetCharacterMovement()->bOrientRotationToMovement = true; // Character moves in the direction of input...	
 	GetCharacterMovement()->RotationRate = FRotator(0.0f, 540.0f, 0.0f); // ...at this rotation rate
-	GetCharacterMovement()->JumpZVelocity = 600.f;
+	GetCharacterMovement()->JumpZVelocity = 300.f;
 	GetCharacterMovement()->AirControl = 0.2f;
 
 	// Create a camera boom (pulls in towards the player if there is a collision)
